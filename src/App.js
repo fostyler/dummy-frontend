2b0a5e15c37a5d79a103d33993a4d464db302f1e
Tyler Barabas <tylerbarabas@gmail.com>
2023-03-31 15:07:33 -0500

    assignment finished

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import { useState, useEffect } from 'react'
-import BabyService from './services/baby'
+import BabyList from './components/baby-list'
 
 function App() {
-  const [ babyList, setBabyList ] = useState([])
-  useEffect(()=>{
-    const getList = async()=>{
-      const list = await BabyService.getBabyList()
-      setBabyList(list.babies)
-      console.log('list', list.babies)
-    }
-    getList()
-  },[])
-
-  return (
-    <div>
-        <table border="1">
-          <tr>
-            <td>First Name</td>
-            <td>Middle Name</td>
-            <td>Last Name</td>
-          </tr>
-          {Array.isArray(babyList) ? babyList.map((baby)=>(
-            <tr>
-              <td>{baby.first_name}</td>
-              <td>{baby.middle_name}</td>
-              <td>{baby.last_name}</td>
-            </tr>
-          )):null}
-        </table>
-    </div>
-  );
+  return <BabyList />
 }
 
 export default App;
---
+++ src/components/baby-list/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import BabyService from '../../services/baby'
+function BabyList(){
+  const [ babyList, setBabyList ] = useState([])
+
+  useEffect(()=>{
+    const getList = async()=>{
+      const list = await BabyService.getBabyList()
+      setBabyList(list.babies)
+    }
+    getList()
+  },[])
+
+  return (
+    <div>
+        <table border="1" style={{margin: '0 auto', fontSize: 32}}>
+          <tr>
+            <td>First Name</td>
+            <td>Middle Name</td>
+            <td>Last Name</td>
+          </tr>
+          {Array.isArray(babyList) ? babyList.map((baby)=>(
+            <tr>
+              <td>{baby.first_name}</td>
+              <td>{baby.middle_name}</td>
+              <td>{baby.last_name}</td>
+            </tr>
+          )):null}
+        </table>
+    </div>
+  );
+}
+
+export default BabyList
